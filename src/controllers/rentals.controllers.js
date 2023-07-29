e555f1b0cbb7289008b1aa0a5335c0455654d9e0
Yoosephh <jose.es.oleg@gmail.com>
2023-07-29 19:17:59 -0300

    fix: rentClosed assignment as const

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -10,7 +10,10 @@ export async function sendRents(req,res) {
       rentals."gameId",
       TO_CHAR(rentals."rentDate", 'YYYY-MM-DD') AS "rentDate",
       rentals."daysRented",
-      rentals."returnDate",
+      CASE 
+        WHEN rentals."returnDate" IS NULL THEN NULL
+        ELSE TO_CHAR(rentals."returnDate", 'YYYY-MM-DD')
+      END AS "returnDate",
       rentals."originalPrice",
       rentals."delayFee",
       JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS "customer",
@@ -56,33 +59,32 @@ export async function newRent(req,res) {
 
 export async function closeRent(req, res) {
   const { id } = req.params;
-  const { customerId, gameId } = req.body;
 
   try {
     const checkRent = await db.query(
-      `SELECT id, "delayFee","daysRented","originalPrice", TO_CHAR("rentDate", 'YYYY-MM-DD') AS "rentDate" FROM rentals WHERE id = $1`,
+      `SELECT id,"customerId", "gameId", "delayFee","daysRented","originalPrice", TO_CHAR("rentDate", 'YYYY-MM-DD') AS "rentDate" FROM rentals WHERE id = $1`,
       [id]
     );
-    const {delayFee, daysRented, originalPrice, rentDate} = checkRent.rows[0]
+    let {delayFee, daysRented, originalPrice, rentDate, customerId, gameId} = checkRent.rows[0]
 
     if (!checkRent.rows[0]) return res.status(404).send("Não é possível finalizar um aluguel que não foi registrado!");
     if (delayFee) return res.status(400).send("Não é possível finalizar um aluguel que já foi finalizado!");
 
     const diaAlugel = dayjs(rentDate);
     const pricePerDay = (originalPrice / daysRented)
-
+    const returnDate = dayjs().format('YYYY-MM-DD')
     const shouldReturnDay = diaAlugel.add(daysRented, 'day').format('YYYY-MM-DD');
     const delayDays = dayjs().diff(shouldReturnDay, 'days');
     delayFee = delayDays > 0 ? delayDays * pricePerDay : 0;
 
     originalPrice = originalPrice + delayFee
-
+    console.log(returnDate)
     await db.query(
       `UPDATE rentals SET "returnDate" = $1, "delayFee" = $2, "originalPrice" = $5 WHERE "customerId" = $3 AND "gameId" = $4`,
-      [dayjs().format('YYYY-MM-DD'), delayFee, customerId, gameId, originalPrice]
+      [returnDate, delayFee, customerId, gameId, originalPrice]
     );
 
-    await db.query(`UPDATE games SET "stockTotal" += 1 WHERE id = $1`, [gameId]);
+    await db.query(`UPDATE games SET "stockTotal" = "stockTotal" + 1 WHERE id = $1`, [gameId]);
 
     res.status(200).send("Devolução efetuada com sucesso!");
   } catch (err) {
