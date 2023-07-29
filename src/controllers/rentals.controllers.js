05ad643e3f108e9d486ccaf4ef445045769ca1bc
Yoosephh <jose.es.oleg@gmail.com>
2023-07-29 11:03:26 -0300

    update: route rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -3,7 +3,24 @@ import { db } from "../database/database.js"
 
 export async function sendRents(req,res) {
   try{
-    const rents = await db.query(`SELECT id, customerId, "gameId", TO_CHAR("rentDate", 'YYYY-MM-DD') AS "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee", customer, game  FROM rentals`)
+    const rents = await db.query(`
+    SELECT
+      rentals.id,
+      rentals."customerId",
+      rentals."gameId",
+      TO_CHAR(rentals."rentDate", 'YYYY-MM-DD') AS "rentDate",
+      rentals."daysRented",
+      rentals."returnDate",
+      rentals."originalPrice",
+      rentals."delayFee",
+      JSON_BUILD_OBJECT('id', customers.id, 'name', customers.name) AS "customer",
+      JSON_BUILD_OBJECT('id', games.id, 'name', games.name) AS "game"
+    FROM rentals
+    JOIN games
+      ON rentals."gameId" = games.id
+    JOIN customers
+      ON rentals."customerId" = customers.id
+  `);
 
     res.status(200).send(rents.rows)
     
@@ -19,10 +36,18 @@ export async function newRent(req,res) {
     if (Number(daysRented) <= 0) return res.status(400).send("O numero de dias alugados está inconsistente")
     const checkCustomer = await db.query(`SELECT id FROM customers WHERE id = $1`, [customerId])
     const checkGame = await db.query(`SELECT id, "stockTotal", "pricePerDay" FROM games WHERE id = $1`, [gameId])
-    if(checkCustomer.rows.length === 0 || checkGame.rows.length === 0) return res.sendStatus(400)
-    if(checkGame.rows[0].stockTotal >= 0) return res.sendStatus(400)
+    
+    if(checkCustomer.rows.length === 0 || checkGame.rows.length === 0) return res.status(400).send("Falhou aqui")
+    console.log(checkGame.rows[0].stockTotal)
+    if(checkGame.rows[0].stockTotal <= 0) return res.status(400).send("O jogo selecionado está indisponível para aluguel")
 
-    await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "delayFee", "originalPrice") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`, [customerId, gameId, daysRented, dayjs().format('YYYY-MM-DD'), null, null, (checkGame.rows[0].pricePerDay * daysRented) ])
+    await db.query(`
+      INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "delayFee", "originalPrice") 
+      VALUES ($1, $2, $3, $4, $5, $6, $7) 
+      RETURNING id`, 
+      [customerId, gameId, daysRented, dayjs().format('YYYY-MM-DD'), null, null, (checkGame.rows[0].pricePerDay * daysRented)])
+
+    await db.query(`UPDATE games SET "stockTotal" = $1 WHERE id = $2`, [(checkGame.rows[0].stockTotal - 1), gameId] )
 
     res.status(201).send("Reserva realizada com sucesso! Divirta-se com os jogos :D")
   } catch(err){
@@ -35,9 +60,12 @@ export async function closeRent(req,res) {
 
   try{
     if (Number(daysRented) <= 0) return res.status(400).send("O numero de dias alugados está inconsistente")
+
     const checkCustomer = await db.query(`SELECT id FROM customers WHERE id = $1`, [customerId])
     const checkGame = await db.query(`SELECT id, "stockTotal", "pricePerDay" FROM games WHERE id = $1`, [gameId])
+
     if(checkCustomer.rows.length === 0 || checkGame.rows.length === 0) return res.sendStatus(400)
+
     if(checkGame.rows[0].stockTotal >= 0) return res.sendStatus(400)
 
     await db.query(`INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "delayFee", "originalPrice") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`, [customerId, gameId, daysRented, dayjs().format('YYYY-MM-DD'), null, null, (checkGame.rows[0].pricePerDay * daysRented) ])
@@ -51,6 +79,7 @@ export async function closeRent(req,res) {
 export async function deleteRent(req,res) {
   try{
 
+    return res.status(200).send("Registro deletado com sucesso!")
   } catch(err){
     console.log(err)
   }
--- src/routes/rentals.routes.js
+++ src/routes/rentals.routes.js
@@ -1,11 +1,11 @@
 import {Router} from "express"
-import { closeRent, newRent, sendRents } from "../controllers/rentals.controllers.js";
+import { closeRent, deleteRent, newRent, sendRents } from "../controllers/rentals.controllers.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals", newRent)
 rentalsRouter.get("/rentals", sendRents)
 rentalsRouter.put("/rentals/:id/return", closeRent)
-// rentalsRouter.delete("/rentals/:id")
+rentalsRouter.delete("/rentals/:id", deleteRent)
 
 export default rentalsRouter;
