e6fc71b6210c30d0acc47dc24a300414f5864ec3
Yoosephh <jose.es.oleg@gmail.com>
2023-07-28 22:39:26 -0300

    Fix: clients route fix

--- src/controllers/clients.controllers.js
+++ src/controllers/clients.controllers.js
@@ -16,8 +16,8 @@ export async function showCustomer(req,res) {
   const { id } = req.params
 
   try{
-    const customer = await db.query(`SELECT * FROM customers WHERE customers.id = $1`, [id])
-    if (!customer) return res.status(404).send("Usuario não encontrado no sistema!")
+    const customer = await db.query(`SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday FROM customers WHERE customers.id = $1`, [id])
+    if (customer.rows.length === 0) return res.status(404).send("Usuario não encontrado no sistema!")
 
     res.status(200).send(customer.rows[0])
 
@@ -44,11 +44,11 @@ export async function updateCustomer(req, res) {
   const { name, phone, cpf, birthday } = req.body
   const { id } = req.params
   try{
-    const user = await db.query(`SELECT * FROM customers WHERE id = $1`, [id])
+    const user = await db.query(`SELECT id, name, phone, cpf, TO_CHAR(birthday, 'YYYY-MM-DD') AS birthday FROM customers WHERE id = $1`, [id])
 
-    if(!user || !user.cpf) return res.status(400).send("Verifique seus dados e tente novamente!")
+    if(user.rows.length === 0) return res.status(400).send("Verifique seus dados e tente novamente!")
 
-    if (user.cpf !== cpf) return res.status(409).send("Verifique seus dados e tente novamente!")
+    if (user.rows.cpf !== cpf) return res.status(409).send("Verifique seus dados e tente novamente!")
 
     await db.query(`UPDATE customers name = $1, phone = $2, birthday = $3 WHERE id = $4`, [name, phone, birthday, id])
   } catch (err) {
